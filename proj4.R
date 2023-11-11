781c1056a01cbdd4dbd3306ca598473f7691b763
Michelle(Zheyue) <zheyue129@gmail.com>
2023-11-11 18:12:33 +0000

    task2 and task3

--- proj4.R
+++ proj4.R
@@ -1,3 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Write a function backward(nn,k) for computing the derivatives of the loss 
 # corresponding to output class k for network nn (returned from forward). 
 # Derivatives w.r.t. the nodes, weights and offsets should be computed and added
@@ -5,30 +20,68 @@
 # object
 
 # "f_" marks h, W and b returned from forward function.
-# f_h: a list of nodes for each layer. f_h[[l]] is a vector of length d[l]
-# which contains the node values for layer l.
-# f_W: a list of weight matrices. f_W[[l]] is the weight matrix linking layer l 
-# to layer l+1.
-# f_b: a list of offset vectors. b[[l]] is the offset vector linking layer l to 
-# layer l+1.
-c(f_h, f_W, f_b) <- forward(nn, inp)
+f_nn <- forward(nn, inp)
 
-# k is an integer representing class
+# nn: network returned from forward function
+# k: an integer representing class
 backward<-function(nn, k){
-  # First, calculate derivative of the loss for k_i w.r.t h^L_j: dlj
-  # L: the number of layers in total
+  # f_h: a list of nodes for each layer. f_h[[l]] is a vector of length d[l] 
+  # which contains the node values for layer l.
+  # f_W: a list of weight matrices. f_W[[l]] is the weight matrix linking layer l 
+  # to layer l+1.
+  # f_b: a list of offset vectors. b[[l]] is the offset vector linking layer l to 
+  # layer l+1.
+  f_h <- nn$h
+  f_W <- nn$W
+  f_b <- nn$b
+  # The number of nodes
   L <- length(f_h)
-  for (l in range(L)){
-    hl <- f_h[[l]]
-    # Apply exp() to all elements in h_l 
-    hlq_exp <- sapply(hl, exp)
-    sumq <- sum(hlq_exp)
+  # Define a function to calculate dlj by nodes for layer l
+  # hl is a vector containing node values for layer l
+  cal_dlj<- function(hl, k) {
+    # Calculate derivatives of the loss for ki w.r.t hlj: Dl[j]
+    # "sapply" applies exp() to all nodes in hl and return a vector that the q-th
+    # element in the vector is exp(hl[q]), then sum up all exp(hl[q])
+    sumq <- sum(sapply(hl, exp))
+    # Except for j=k, derivative of the loss of ki w.r.t hlj is 
+    # exp(hl[j])/sum(exp(hl[q]))
     Dl = hl/sumq
+    # When j=k, derivative is equal to exp(hl[j])/(sum(exp(hl[q]))-1)
     Dl[k] = hl[k]/(sumq-1)
-    dl = pmax(Dl, 0)
+    # If hl[j] <= 0, dl[j] = 0; otherwise, dl[j] = Dl[j]
+    dl <- Dl
+    dl[which(hl < 0)] <- 0
+    return (dl)
   }
-    
+  # "lapply" will apply cal_dlj function to each sublist in f_h, the vectors hl 
+  # for each layer, and return a new list with sublists containing dl for each 
+  # layer.
+  d <- lapply(f_h, cal_dlj)
+  # Then, compute the derivatives of Li w.r.t all the other hl[j] by working
+  # backwards through layers applying the chain rule
+  # "lapply" will make all sublists in f_W, weight matrices, transpose and return
+  # a new list containing transposed matrices
+  tW <- lapply(f_W, t)
+  adjusted_tW <- tW[1:L-1]
+  adjusted_d <- d[2:L]
+  # "mapply" use function crossprod() on each sublist in adjusted_tW and adjusted_d
+  # equally, dh[i] = adjusted_tW[i] %*% adjusted_d[i] for all i
+  dh <- mapply(crossprod, adjusted_tW, adjusted_d)
   
-  result = list("h" = b_h, "W" = b_W, "b" = b_b, "dh" = dh, "dW" = dW, "db" = db)
-  return (result)
+  db <- adjusted_d
+  
+  # "lapply" will make all sublists in adjusted_f_h, derivative of the loss, 
+  # transpose and return a new list containing transposed matrices
+  # "mapply" use function crossprod() on each sublist in adjusted_d and adjusted_tf_h
+  # equally, dW[i] = adjusted_d[i] %*% adjusted_tf_h[i] for all i
+  # Set "SIMPLIFY = FALSE" to make the function return a list
+  adjusted_tf_h <- lapply(f_h[1:L-1], t)
+  dW <- mapply(crossprod, adjusted_d, adjusted_tf_h, SIMPLIFY = FALSE)
+    
+  # Update the network lists
+  network = list("h" = f_h, "W" = f_W, "b" = f_b, "dh" = dh, "dW" = dW, "db" = db)
+  return (network)
 }
+
+# "b_" marks the network list returned from backward function
+b_nn <- backward(f_nn, k)
